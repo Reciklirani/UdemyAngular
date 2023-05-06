f8f577ad330e5479fa853a3c86a0b71e17e05650
Vladimir Kantarevic <kantarevicvladimir@gmail.com>
2023-05-06 23:13:06 +0200

    completed routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -34,6 +34,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   addIngredientsToCart(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredinets(ingredients);
   }
